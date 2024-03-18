10ece13121a5b15d2f8c7d63fe27e7d6b9f5672d
Prince <princejaj67@gmail.com>
2024-03-18 17:27:32 +0530

    Netlify

--- src/Components/AllNews.js
+++ src/Components/AllNews.js
@@ -117,9 +117,9 @@ export class AllNews extends Component {
           <button style={{marginRight : "2vh" ,marginLeft: "2vh",borderRadius: "12vh",padding : "2vh"}} onClick={this.refr}>REFRESH</button>
           </div>
         <InfiniteScroll
-          dataLength={this.state.articles.length}
+          dataLength={this.state.articles.length != 0 ? this.state.articles.length : 8}
           next={this.fetchMoreData}
-          hasMore={this.state.articles.length !== this.state.totalNo}
+          hasMore={this.state.articles.length != 0 ? this.state.articles.length : 8 !== this.state.totalNo}
           style={{ display: 'flex', flexDirection: 'column-reverse' }} 
           // inverse={true} 
           // loader={<h1>LOADING </h1>}
