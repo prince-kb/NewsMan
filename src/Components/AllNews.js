118dc262463716107136936146db8e1039b2097c
Prince <princejaj67@gmail.com>
2024-03-19 12:31:52 +0530

    Hosting to vercell trial 1

--- src/Components/AllNews.js
+++ src/Components/AllNews.js
@@ -143,9 +143,9 @@ export class AllNews extends Component {
           <button style={{marginRight : "2vh" ,marginLeft: "2vh",borderRadius: "12vh",padding : "2vh"}} onClick={this.refr}>REFRESH</button>
           </div>
         <InfiniteScroll
-          dataLength={this.state.articles.length}
+          dataLength={this.state.articles ? this.state.articles.length : 8}
           next={this.fetchMoreData}
-          hasMore={this.state.articles.length !== this.state.totalNo}
+          hasMore={this.state.articles ? this.state.articles.length : 8 !== this.state.totalNo}
           style={{ display: 'flex', flexDirection: 'column-reverse' }} 
           // inverse={true} 
           // loader={<L/>}
